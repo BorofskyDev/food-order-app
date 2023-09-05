10e32fc3a61a3eeb81380e4c3b2c7b33662323f0
solovyovsdom <joelborofsky@mac.com>
2023-09-05 08:57:06 -0500

    Added header and meals styling and basic jsx

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,19 @@
+import { Fragment } from 'react'
+import photo from '../../assets/meals.jpg'
+import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton'
+
+function Header(props) {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={photo} alt='a table full of food' />
+      </div>
+    </Fragment>
+  )
+}
+export default Header
